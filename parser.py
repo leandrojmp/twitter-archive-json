0a651a1ae9f63bcae4f04b320481048866458a5c
Leandro Maciel <leandrojmp@gmail.com>
2019-08-25 01:44:41 -0300

    changes on parser and pipeline (#1 and #3)

--- parser.py
+++ parser.py
@@ -1,6 +1,9 @@
 # parser.py
 # 
-# parse the tweet archive file
+# parse the tweet archive file, each tweet will be in one line
+#
+# usage: parse.py /path/to/tweet.js
+# the output is saved as json/tweet.js
 
 import json
 import os
@@ -9,51 +12,19 @@ import calendar
 from sys import argv
 
 # input and output files
+src_file = open(argv[1],'r') 
+parsed_file = 'json/' + os.path.basename(str(src_file)).split('.')[0] + '.json'
 
-src_dir = argv[1]
 
-# create days and months lists
-days = list(calendar.day_name)
-months = list(calendar.month_name)
+# need to skip the first 25 characters on each file
+# the first 25 chars are something like: window.YTD.tweet.part0 = [
+src_file.seek(25)
 
-for f in os.listdir(src_dir):
-    json_file = 'json/' + os.path.basename(f).split('.')[0] + '.json'
-    # first tests, pass the file as an argument
-    with open(src_dir + f,'r') as j_file:
-        next(j_file)
-        j_data = json.loads(j_file.read())
-
-    with open(json_file,'a') as out_file:
-        for i in j_data:
-            tweets = {}
-            tweet_users = []
-            tweets['tweet.id'] = str(i['id'])
-            tweets['tweet.timestamp'] = i['created_at']
-            # create a date time variable to extract the year, the day of week and the month
-            tweet_date = datetime.datetime.strptime(tweets['tweet.timestamp'], '%Y-%m-%d %H:%M:%S +0000')
-            tweets['tweet.year'] = tweet_date.year
-            tweets['tweet.day'] = days[tweet_date.weekday()].lower()
-            tweets['tweet.month'] = months[tweet_date.month].lower()
-            # 
-            tweets['tweet.source'] = i['source'].split('>')[1].split('<')[0].lower()
-            tweet = i['text'].replace('\n','').replace('"','').replace('\'','').replace('\\','')
-            tweets['tweet.length'] = len(tweet)
-            tweets['tweet.text'] = tweet
-            if tweet[:1] == "@":
-                tweets['tweet.type'] = "reply"
-            elif tweet[:3] == "RT ":
-                tweets['tweet.type'] = "retweet"
-            else:
-                tweets['tweet.type'] = "tweet"
-            if tweets['tweet.type'] == "reply":
-                for u in tweet.split():
-                    if u[0] == "@":
-                        tweet_users.append(u)
-                tweets['tweet.users'] = tweet_users
-            if "coordinates" in i['geo']:
-                geo_lat = i['geo']['coordinates'][0]
-                geo_lon = i['geo']['coordinates'][1]
-                tweets['tweet.geo'] = str(geo_lat) + "," + str(geo_lon)
-            
-            json.dump(tweets, out_file)
-            out_file.write('\n')
+# reads the file and parse the json
+archive_data = src_file.read()
+json_data = json.loads(archive_data)
+# save each tweet as a line in the output file
+with open(parsed_file,'a') as dst_file:
+    for tweet in json_data:
+        json.dump(tweet,dst_file)
+        dst_file.write('\n')
